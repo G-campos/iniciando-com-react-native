1608fe4beacd3a8e396f2131b3dcfe66bf36beec
Gabriel de Campos <gabrielcampos@eulabs.com.br>
2022-01-17 16:44:50 -0400

    Mudando de pasta

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,8 @@ import {
   FlatList
 } from 'react-native';
 
-import {Button} from './components/Button';
-import {SkillCard} from "./components/SkillCard";
+import {Button} from '../components/Button';
+import {SkillCard} from "../components/SkillCard";
 
 export function Home() {
   const [newSkill, setNewSkill] = useState('');
