619a9a57eaa0673ca323bbad3f73d153c3527357
Gabriel de Campos <gabrielcampos@eulabs.com.br>
2022-01-17 20:10:38 -0400

    Refatoração de js para ts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,13 +11,23 @@ import {
 import {Button} from '../components/Button';
 import {SkillCard} from "../components/SkillCard";
 
+interface SkillData {
+  id: String;
+  name: String;
+}
+
 export function Home() {
   const [newSkill, setNewSkill] = useState('');
-  const [mySkills, setMySkills] = useState([]);
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
   const [gretting, setGretting] = useState('');
 
   function handleAddNewSkill() {
-    setMySkills(oldState => [...oldState, newSkill]);
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill
+    }
+
+    setMySkills(oldState => [...oldState, data]);
   }
 
   useEffect(() => {
@@ -44,15 +54,18 @@ export function Home() {
         onChangeText={setNewSkill}
       />
 
-      <Button onPress={handleAddNewSkill}/>
+      <Button
+        title="Add"
+        onPress={handleAddNewSkill}
+      />
 
       <Text style={[styles.title, {marginVertical: 50}]}>My Skills</Text>
 
       <FlatList
         data={mySkills}
-        keyExtractor={item => item}
+        keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <SkillCard skill={ item } />
+          <SkillCard skill={ item.name } />
         )}
       />
 
