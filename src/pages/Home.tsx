a67011cf3b6c88e23a7f63797baeb4af37ba9d36
Gabriel de Campos <gabrielcampos@eulabs.com.br>
2022-01-18 19:03:56 -0400

    Refatoração de js para ts e finalização do modulo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,6 +30,12 @@ export function Home() {
     setMySkills(oldState => [...oldState, data]);
   }
 
+  function handleRemoveSkill(id: String){
+    setMySkills(oldState => oldState.filter(
+      skill => skill.id !== id
+    ));
+  }
+
   useEffect(() => {
     const currentHour = new Date().getHours();
      if(currentHour < 12 ){
@@ -39,7 +45,7 @@ export function Home() {
      } else {
        setGretting('Boa noite');
      }
-  }, [mySkills])
+  }, [])
 
   return (
     <View style={styles.container}>
@@ -65,7 +71,10 @@ export function Home() {
         data={mySkills}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <SkillCard skill={ item.name } />
+          <SkillCard
+            skill={ item.name }
+            onPress={() => handleRemoveSkill(item.id)}
+          />
         )}
       />
 
